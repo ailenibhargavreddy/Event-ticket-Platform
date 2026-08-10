a5b5b5f0aab2d9958635269ea38c6ea8acb30fcf
ailenibhargavreddy <ailenibhargavreddy1@gmail.com>
2026-08-10 05:48:19 +0530

    created Ticket and TicketType entities

---
+++ src/main/java/com/bhargav/tickets/domain/Ticket.java
@@ -0,0 +1,46 @@
+package com.bhargav.tickets.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ticket {
+    @Id
+    @Column(name="id",nullable = false,updatable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name="status",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketStatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticker_type_id")
+    private TicketType ticketType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="purchaser_id")
+    private User purchaser;
+    //TODO: validation
+    //TODO: QR Code
+    @CreatedDate
+    @Column(name="create_at",updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name="updated_at",nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
--- src/main/java/com/bhargav/tickets/domain/User.java
+++ src/main/java/com/bhargav/tickets/domain/User.java
@@ -1,15 +1,14 @@
 package com.bhargav.tickets.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -22,17 +21,40 @@ import java.util.UUID;
 public class User {
     @Id
     private UUID id;
+
     @Column(name="name",nullable = false,updatable = false)
     private String name;
+
     @Column(name = "email",nullable = false)
     private String email;
-    //TODO: Organized events
-    //TODO:  Attending events
-    //TODO: Staffing events
+
+    @OneToMany(mappedBy = "organizer",cascade = CascadeType.ALL)
+    private List<Event> organizedEvents = new ArrayList<>();
+
+
+    @ManyToMany
+    @JoinTable(
+            name="user_attending_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> attendingEvents = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_staffing_events",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> staffingEvents = new ArrayList<>();
+
+
+
     @CreatedDate
     @Column(name="create_at",updatable = false,nullable = false)
     private LocalDateTime createdAt;
+
     @LastModifiedDate
     @Column(name="updated_at",nullable = false)
-    private LocalDateTime updated;
+    private LocalDateTime updatedAt;
 }
